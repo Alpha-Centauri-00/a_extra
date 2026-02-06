503cdd7a79fa8d7e9fc2c07fb005b2055b29c7c5
alpha <alpha@example.com>
2026-02-06 11:46:40 +0100

    create constants

---
+++ config/constants.py
@@ -0,0 +1,15 @@
+############## CONSTANTS ##############
+
+G = 6.674e-11          # m^3 / (kg s^2)
+C = 3.0e8              # m/s
+C_SQ = C ** 2
+G_OVER_C2 = G / C_SQ
+
+KPC_TO_M = 3.086e19
+MPC_TO_M = 3.086e22
+M_SUN = 1.989e30
+
+GYR_TO_S = 1e9 * 365.25 * 24 * 3600
+HUBBLE_H = 0.7         # your assumed h
+R0_KPC = 0.5           # galaxy softening radius
+R0_M = R0_KPC * KPC_TO_M
